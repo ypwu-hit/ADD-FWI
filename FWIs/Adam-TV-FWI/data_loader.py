a9f1611272e87a2cd03bbfecf2959051cdab8ada
ypwu-hit <ypwu@stu.hit.edu.cn>
2025-03-28 13:28:09 +0800

    Add files via upload

---
+++ FWIs/Adam-TV-FWI/data_loader.py
@@ -0,0 +1,130 @@
+# @author: wuyuping (ypwu@stu.hit.edu.cn)
+
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import deepwave
+from deepwave import scalar
+from scipy.signal import butter
+from torchaudio.functional import biquad
+
+def DataLoad_Train(n_shots):
+
+    # Set default dtype to float32
+    torch.set_default_dtype(torch.float)
+
+    # PyTorch random number generator
+    torch.manual_seed(1234)
+
+    # Random number generators in other libraries
+    np.random.seed(1234)
+
+    # device = torch.device('cuda:0')
+    # device = torch.device('cpu')
+
+    data_path = '../../data/'
+    
+    ny = 340
+    nx = 130
+
+    v_true = torch.from_file(data_path+'mar_big_vp_130_340.bin',
+                        size=ny * nx).reshape(ny, nx)
+
+    dx = 22.5 # 35
+    
+    Vvmax = torch.max(v_true)
+    Vvmin = torch.min(v_true)
+    print('vmin, vmax', Vvmin, Vvmax)
+
+    model_dim = v_true.shape
+    print('v_true.shape', v_true.shape)
+
+    plt.figure()
+    plt.imshow(v_true.T, aspect='auto', cmap='jet')
+    plt.xlabel("X")
+    plt.ylabel("Z")
+    plt.title("Velocity")
+    plt.colorbar()
+
+    plt.tight_layout()
+    plt.savefig('example_v.png')
+    plt.close()
+
+    n_shots = n_shots
+    
+    n_sources_per_shot = 1
+    d_source = 11  # 10 * 8m = 80m
+    first_source = 5  # 5 * 8m = 40m
+    source_depth = 1  # 1 * 8m = 8m
+
+    n_receivers_per_shot = 339
+    d_receiver = 1  # 3 * 8m = 24m
+    first_receiver = 0  # 0 * 8m = 0m
+    receiver_depth = 1  # 1 * 8m = 8m
+
+    freq = 7
+    nt = 2000
+    dt = 0.002
+    peak_time = 1.0 / freq
+
+    print(deepwave.common.cfl_condition(dy = dx, dx = dx, dt = dt, max_vel = 4700))
+
+    # source_locations, [shot, source, space]
+    source_locations = torch.zeros(n_shots, n_sources_per_shot, 2,
+                                   dtype=torch.long)
+    source_locations[..., 1] = source_depth
+    source_locations[:, 0, 0] = torch.arange(n_shots) * d_source + first_source
+
+    # receiver_locations [shot, receiver, space]
+    receiver_locations = torch.zeros(n_shots, n_receivers_per_shot, 2,
+                                     dtype=torch.long)
+    receiver_locations[..., 1] = receiver_depth
+    receiver_locations[:, :, 0] = (
+        (torch.arange(n_receivers_per_shot) * d_receiver + first_receiver)
+            .repeat(n_shots, 1)
+    )
+
+    # source_amplitudes [shot, source, time]
+    # source_amplitudes [shot, source, time]
+    source_amplitudes = (
+        deepwave.wavelets.ricker(freq, nt, dt, peak_time)
+    )
+
+    sos = butter(6, 5, 'hp', fs=1/dt, output='sos')
+    sos = [torch.tensor(sosi).to(source_amplitudes.dtype)
+           for sosi in sos]
+
+    def filt(x):
+        return biquad(biquad(biquad(x, *sos[0]), *sos[1]),
+                      *sos[2])
+
+    source_amplitudes_filt = filt(source_amplitudes).repeat(n_shots, n_sources_per_shot, 1)
+
+    # Propagate [num_shots, n_r, n_t]
+    # observed_data = scalar(v_true, dx, dt, source_amplitudes=source_amplitudes,
+    #                        source_locations=source_locations,
+    #                        receiver_locations=receiver_locations,
+    #                        accuracy=8,
+    #                        pml_freq=freq)[-1]
+
+    # Propagate [num_shots, n_r, n_t] observed_data
+    observed_data = (
+    torch.from_file(data_path+'marmousi2_130_340_data_experiments3_3_filt.bin',
+                    size=n_shots*n_receivers_per_shot*nt)
+    .reshape(n_shots, n_receivers_per_shot, nt)
+    )
+    print('observed_data.shape', observed_data.shape)
+    data_dim = observed_data[0].reshape
+
+    noise = (
+    torch.from_file(data_path+'marmousi2_130_340_data_noise_experiments3_3_filt.bin',
+                    size=n_shots*n_receivers_per_shot*nt)
+    .reshape(n_shots, n_receivers_per_shot, nt)
+    )
+    print('noise.shape', noise.shape)
+    
+    observed_data_noise = observed_data + noise
+
+    return observed_data, observed_data_noise, data_dim, model_dim, source_amplitudes_filt, source_locations, receiver_locations, v_true
+
+# DataLoad_Train(30)
---
+++ FWIs/Adam-TV-FWI/train.py
@@ -0,0 +1,277 @@
+# @author: wuyuping (ypwu@stu.hit.edu.cn)
+
+import time
+import torch
+import numpy as np
+from data_loader import DataLoad_Train
+import matplotlib.pyplot as plt
+import torch.utils.data as data_utils
+from deepwave import scalar
+import scipy.io
+from scipy.ndimage import gaussian_filter
+from pytorch_msssim import MS_SSIM, ms_ssim, SSIM, ssim
+import random
+
+class SSIM_Loss(SSIM):
+    def forward(self, img1, img2):
+        return (super(SSIM_Loss, self).forward(img1, img2) )
+
+class MyDataset(data_utils.Dataset):
+    def __init__(self,seismic_data, source_amplitudes, source_locations, receiver_locations):
+
+        self.seismic_data = seismic_data
+        self.source_amplitudes = source_amplitudes
+        self.source_locations = source_locations
+        self.receiver_locations = receiver_locations
+
+    def __getitem__(self, item):
+
+        seismic_data = self.seismic_data[item]
+        source_amplitudes = self.source_amplitudes[item]
+        source_locations = self.source_locations[item]
+        receiver_locations = self.receiver_locations[item]
+
+        return seismic_data, source_amplitudes, source_locations, receiver_locations
+
+    def __len__(self):
+        return len(self.seismic_data)
+
+def same_seeds(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    np.random.seed(seed)  # Numpy module.
+    random.seed(seed)  # Python random module.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True  
+
+# @author: fangshuyang (yangfs@hit.edu.cn)
+def ATVLoss(x):    
+    """Compute L1-based TV loss for x
+
+    Args:
+        x: image, torch.Variable of torch.Tensor
+    Returns:
+           ATV loss
+
+    alpha_tv * (ATVLoss(model))/(ny*nz)
+
+    """
+
+    dh       = x[:,1:] - x[:,:-1]
+    dw       = x[1:,:] - x[:-1,:]
+    atvloss  = torch.sum(torch.abs(dh[:-1,:]) + torch.abs(dw[:, :-1]))
+
+    return atvloss
+
+same_seeds(1234)
+
+# Here indicating the GPU you want to use. if you don't have GPU, just leave it.
+cuda_available = torch.cuda.is_available()
+device = torch.device('cuda:1')
+torch.set_default_dtype(torch.float)
+
+BatchSize = 6
+number_of_shots = 30
+dx = 22.5
+dt = 0.002
+freq = 7
+
+seismic_data, seismic_data_noise, data_dim, model_dim, source_amplitudes, x_s, x_r, v_true = DataLoad_Train(number_of_shots)
+# print('source_amplitudes', source_amplitudes.shape, x_s.shape, x_r.shape)
+
+model_max = torch.max(v_true)
+model_min = torch.min(v_true)
+v_true_nor = (v_true - model_min) / (model_max - model_min)
+v_true_nor = v_true_nor.to(device)
+
+train_loader = data_utils.DataLoader(MyDataset(seismic_data_noise, source_amplitudes, x_s, x_r),
+                                     batch_size=BatchSize,
+                                     shuffle=True, drop_last=True)
+
+v_init = (torch.tensor(1/gaussian_filter(1/v_true.cpu().numpy(), 30)).to(device))
+v_true = v_true.to(device)
+
+plt.figure()
+plt.imshow(v_init.detach().cpu().numpy().T, aspect='auto', cmap='jet', vmax=4700, vmin=1100)
+plt.xlabel("X")
+plt.ylabel("Z")
+plt.title("v_init")
+plt.colorbar()
+plt.tight_layout()
+plt.savefig('v_init.png')
+plt.close()
+
+v = v_init.clone()
+v.requires_grad_()
+
+# Setup optimiser to perform inversion
+optimizer = torch.optim.Adam([v], lr=25)
+loss_fn = torch.nn.MSELoss()
+loss_ssim = SSIM_Loss(data_range=1.0, size_average=True, channel=1)
+
+print()
+print('*******************************************')
+print('*******************************************')
+print('           START TRAINING                  ')
+print('*******************************************')
+print('*******************************************')
+print()
+
+DisplayStep = 2
+loss_data_file = 'loss_data.txt'
+loss_model_file = 'loss_model.txt'
+loss_ssim_file = 'loss_ssim.txt'
+step   = int(number_of_shots/BatchSize)
+num_epochs = 30000
+start  = time.time()
+alpha_tv = 1e-4
+
+since = time.time()
+for epoch in range(num_epochs):
+
+    epoch_loss = 0.0
+
+
+    for i, (s_d_i, s_a_i, x_s_i, x_r_i) in enumerate(train_loader):
+        optimizer.zero_grad()
+        
+        # Forward prediction
+        out = scalar(
+            v, dx, dt,
+            source_amplitudes=s_a_i.to(device),
+            source_locations=x_s_i.to(device),
+            receiver_locations=x_r_i.to(device),
+            max_vel=4700,
+            pml_freq=freq,
+            accuracy=8,
+            pml_width=[20, 20, 0, 20]
+        )[-1]
+
+        loss = (
+            loss_fn(out.to(device),
+                    s_d_i.to(device)) + alpha_tv * (ATVLoss(v))/(model_dim[0]*model_dim[1])
+        )
+
+        if np.isnan(float(loss.item())):
+            raise ValueError('loss is nan while training')
+
+        epoch_loss += loss.item()
+        # Loss backward propagation
+        loss.backward()
+
+        optimizer.step()
+        v.data = torch.clamp(v.data, min=1100, max=4700)
+
+
+    if epoch % 20 == 0:
+
+        # val
+        with torch.no_grad():  # operations inside don't track history
+            # Validation Mode:
+
+            observed_data_fd = scalar(v.to(device), dx, dt,
+                                      source_amplitudes=source_amplitudes.to(device),
+                                      source_locations=x_s.to(device),
+                                      receiver_locations=x_r.to(device),
+                                      max_vel=4700,
+                                      pml_freq=freq,
+                                      accuracy=8,
+                                      pml_width=[20, 20, 0, 20])[-1]
+
+            loss_model = loss_fn(v, v_true) / loss_fn(v_true, torch.zeros_like(v_true))
+
+            if epoch == 0:
+                loss_model_count = loss_model.detach().cpu()
+                v_save = v.detach().cpu().numpy()
+            else:
+                if loss_model.detach().cpu() < loss_model_count:
+                    loss_model_count = loss_model.detach().cpu()
+                    v_save = v.detach().cpu().numpy()
+
+
+            loss_data = loss_fn(observed_data_fd, seismic_data.to(device)) / loss_fn(seismic_data.to(device), 
+                torch.zeros_like(seismic_data.to(device)))
+
+            model_max = torch.max(v)
+            model_min = torch.min(v)
+            model_nor = (v - model_min) / (model_max - model_min)
+            loss_model_ssim = loss_ssim(model_nor.view(1, 1, model_dim[0], model_dim[1]),
+                                        v_true_nor.view(1, 1, model_dim[0], model_dim[1]))
+
+            if epoch == 0:
+                loss_model_sum = loss_model.detach().cpu()
+                loss_data_sum = loss_data.detach().cpu()
+                loss_model_ssim_sum = loss_model_ssim.detach().cpu()
+            else:
+                loss_model_sum = np.append(loss_model_sum, loss_model.detach().cpu())
+                loss_data_sum = np.append(loss_data_sum, loss_data.detach().cpu())
+                loss_model_ssim_sum = np.append(loss_model_ssim_sum, loss_model_ssim.detach().cpu())
+
+            with open(loss_data_file, 'a') as fr:
+                fr.write(str(loss_data.detach().cpu().numpy()) + '\n')
+            with open(loss_model_file, 'a') as fr:
+                fr.write(str(loss_model.detach().cpu().numpy()) + '\n')
+            with open(loss_ssim_file, 'a') as fr:
+                fr.write(str(loss_model_ssim.detach().cpu().numpy()) + '\n')
+
+            
+            plt.figure()
+            plt.imshow(v.detach().cpu().numpy().T, aspect='auto', cmap='jet', vmax=4700, vmin=1100)
+            plt.xlabel("X")
+            plt.ylabel("Z")
+            plt.title("Velocity")
+            plt.colorbar()
+
+            plt.tight_layout()
+            plt.savefig(str(epoch) + '.png')
+            plt.close()
+
+            v.detach().cpu().numpy().tofile(str(epoch) + 'adam_marmousi2_inv.bin')
+
+            # Print loss and consuming time every epoch
+            # print ('Epoch [%d/%d], Loss: %.10f' % (epoch+1,Epochs,loss.item()))
+            # loss1 = np.append(loss1,loss.item())
+            print('Epoch: {:d} finished ! Data Loss: {:.5f}'.format(epoch + 1, loss_data))
+            print('Epoch: {:d} finished ! Model Loss: {:.5f}'.format(epoch + 1, loss_model))
+            print('Epoch: {:d} finished ! SSIM Model Loss: {:.5f}'.format(epoch + 1, loss_model_ssim))
+            time_elapsed = time.time() - since
+            print('Epoch consuming time: {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+            since = time.time()
+
+v_save.tofile('fin_adam_marmousi2_inv.bin')
+print('lowest model loss is :',loss_model_count)
+# Record the consuming time
+time_elapsed = time.time() - start
+print('Training complete in {:.0f}m  {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
+data = {}
+data['loss_model'] = loss_model_sum
+scipy.io.savemat('ModelLoss.mat', data)
+
+data = {}
+data['loss_data'] = loss_data_sum
+scipy.io.savemat('DataLoss.mat', data)
+
+data = {}
+data['loss_data'] = loss_model_ssim_sum
+scipy.io.savemat('SSIMModelLoss.mat', data)
+
+loss_data_sum = (loss_data_sum - np.min(loss_data_sum)) / (np.max(loss_data_sum) - np.min(loss_data_sum))
+loss_model_sum = (loss_model_sum - np.min(loss_model_sum)) / (np.max(loss_model_sum) - np.min(loss_model_sum))
+
+fig = plt.figure(figsize=(10.5, 3.5), dpi=300)
+plt.plot(np.arange(len(loss_data_sum)), loss_data_sum, label='data loss')
+plt.plot(np.arange(len(loss_model_sum)), loss_model_sum, label='model loss')
+plt.plot(np.arange(len(loss_model_ssim_sum)), loss_model_ssim_sum, label='SSIM model loss')
+plt.legend()
+plt.xlabel('Epoch', fontsize=15)
+plt.ylabel('Loss', fontsize=15)
+# plt.ylim(-0.05,1.05)
+plt.title('Data and model loss', fontsize=15)
+plt.savefig('data_model_loss.jpg', dpi=300, transparent=True, bbox_inches='tight')
+
+
+
+
